df8586d20b5ab8ec59fd12e2684a2ccb6810c7f3
smtkrc <samet.kurucuu@gmail.com>
2021-02-27 11:28:27 +0300

    proje gönderim

---
+++ sayı tahmin/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Chat24012020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SAYI TAHMİN OYUNUNA HOŞ GELDİNİZ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(374, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lütfen bir sayı giriniz";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 114);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(284, 114);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Tekrar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("MS Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(521, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Kontrol et";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button3;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İyi Eğlenceler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ sayı tahmin/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat24012020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayı1 = 0;
+        int sayı2 = 0;
+        int sayac = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rst = new Random();
+            int sayı1 = rst.Next(0, 1000);
+            label1.Visible = false;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            button3.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            textBox1.Visible = true;
+            label2.Visible = true;
+            button3.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sayı2 = int.Parse(textBox1.Text);
+
+                if (sayı1 < sayı2)
+                {
+                    label3.Text = "Daha küçük bir sayı giriniz :)";
+                    sayac += 1;
+                    textBox1.Clear();
+                }
+                if (sayı1 > sayı2)
+                {
+                    label3.Text = "Daha Büyük bir sayı giriniz :)";
+                    sayac += 1;
+                    textBox1.Text = "";
+                }
+                if (sayı1 == sayı2)
+                {
+                    label3.Text = "Tebrikler " + sayac.ToString() + ". deneme de Doğru sayıya ulaştınız";
+                    textBox1.Text = "";
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hatalı giriş yaptınız lütfen bir sayı giriniz:)");
+                textBox1.Text = "";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label3.Text = "başarılar";
+            sayı2 = 0;
+            Random rst = new Random();
+            sayı1 = rst.Next(0, 1000);
+            sayac = 0;
+        }
+    }
+}
